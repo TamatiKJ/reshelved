cbe4a03b303fffe85905aff828c3b938282b4ef9
Tamati <100851598+TamatiKJ@users.noreply.github.com>
2026-05-18 11:11:09 +0300

    Disable admin listings wrapper causing UI lock

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import EditListing from './pages/EditListing';
 import ListingDetail from './pages/ListingDetail';
 import Messages from './pages/Messages';
 import Profile from './pages/Profile';
-import Admin from './pages/AdminListingsManagementWrapper';
+import Admin from './pages/AdminUserDashboardNotifyWrapper';
 import Notifications from './pages/Notifications';
 import LegalPage from './pages/LegalPage';
 
