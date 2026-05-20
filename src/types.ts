128ffaede189bba2342c64ac0270287d02b48b21
Tamati <100851598+TamatiKJ@users.noreply.github.com>
2026-05-20 21:02:07 +0300

    Remove hardcoded listing categories

--- src/types.ts
+++ src/types.ts
@@ -134,21 +134,6 @@ export interface Contact {
   reviewed: boolean;
 }
 
-export const CATEGORIES = [
-  'Fiction',
-  'Fantasy',
-  'Non-Fiction',
-  'Children',
-  'Science & Technology',
-  'Business & Economics',
-  'Arts & Culture',
-  'Self-Help',
-  'Religion & Spirituality',
-  'History',
-  'Biography',
-  'Other'
-];
-
 export const KENYAN_CITIES = [
   'Lavington',
   'Kileleshwa',
