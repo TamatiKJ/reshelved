29bcc6ae893535e63182fa1ff04b6fff4fda281b
Tamati <100851598+TamatiKJ@users.noreply.github.com>
2026-05-27 17:02:15 +0300

    Expose Firebase callable functions client

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app, "reshelved");
+export const functions = getFunctions(app);
 export const storage = getStorage(app);
 export default app;
